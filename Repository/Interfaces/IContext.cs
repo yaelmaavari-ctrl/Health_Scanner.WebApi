2e1495f992f080c0a0e179294058efd08696d47e
Yael <yaelmaavari@gmail.com>
2026-04-13 22:51:48 +0300

    Add IContext Interface

---
+++ Repository/Interfaces/IContext.cs
@@ -0,0 +1,17 @@
+﻿using Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Allergen> Allergens { get; set; }
+        public DbSet<UserAllergen> UserAllergens { get; set; }
+
+    }
+}
