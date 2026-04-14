962d717ed8cacbd07cc3f61973c9cb27d86874a7
Yael <yaelmaavari@gmail.com>
2026-04-14 21:51:26 +0300

    Add UserAllergen repository interface and implementation

--- Repository/Repositories/AllergenRepository.cs
+++ Repository/Repositories/AllergenRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Entities;
+using Repository.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,46 @@ using System.Threading.Tasks;
 
 namespace Repository.Repositories
 {
-    internal class AllergenRepository
+    public class AllergenRepository(IContext context) : IRepository<Allergen>
     {
+        private readonly IContext _context = context;
+        public async Task<Allergen> AddItem(Allergen item)
+        {
+           await _context.Allergens.AddAsync(item);
+           await _context.Save();
+           return item;
+        }
+
+        public async Task<bool> DeleteItem(int id)
+        {
+            var allergen = await _context.Allergens.FirstOrDefaultAsync(a => a.Id == id);
+            if(allergen != null)
+            {
+                _context.Allergens.Remove(allergen);
+                await _context.Save();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<List<Allergen>> GetAll()
+        {
+            return await _context.Allergens.ToListAsync();
+        }
+
+        public async Task<Allergen?> GetById(int id)
+        {
+            return await _context.Allergens.FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public async Task<Allergen?> UpdateItem(int id, Allergen item)
+        {
+            var allergen = await _context.Allergens.FirstOrDefaultAsync(a => a.Id == id);
+            if(allergen == null)
+                return null;
+            allergen.Name = item.Name;
+            await _context.Save();
+            return allergen;
+        }
     }
 }
--- Repository/Repositories/UserRepository.cs
+++ Repository/Repositories/UserRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Entities;
+using Repository.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,45 @@ using System.Threading.Tasks;
 
 namespace Repository.Repositories
 {
-    internal class UserRepository
+    public class UserRepository(IContext context) : IUserRepository
     {
+        private readonly IContext _context = context;
+        public async Task<User> Add(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.Save();
+            return user;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if (user != null) 
+            { 
+                _context.Users.Remove(user);
+                await _context.Save();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<User?> GetByEmail(string email)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+            return user;
+
+        }
+
+        public async Task<User?> Update(int Id, User updatedUser)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == Id);
+            if(user == null)
+                return null;
+            user.Name = updatedUser.Name;
+            user.Email = updatedUser.Email;
+            user.Password = updatedUser.Password;
+            await _context.Save();
+            return user;
+        }
     }
 }
