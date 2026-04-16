383fda48058c6d3d8e9190d7e6efebb6c0679450
Yael <yaelmaavari@gmail.com>
2026-04-16 20:06:54 +0300

    Add custom exceptions class

---
+++ Service/Exceptions/Exceptions.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Service.Exceptions
+{
+    public class NotFoundException(string message) : Exception(message)
+    {
+    }
+
+    public class BadRequestException(string message) : Exception(message)
+    {
+    }
+
+    public class ConflictException(string message) : Exception(message)
+    {
+    }
+
+    public class UnauthorizedException(string message) : Exception(message)
+    {
+    }
+}
