f722a66cca988cbdd8fb9834ea53162d651f3313
Yael <yaelmaavari@gmail.com>
2026-04-22 01:02:50 +0300

    update allergen entity

--- Repository/Entities/Allergen.cs
+++ Repository/Entities/Allergen.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace Repository.Entities
     public class Allergen
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
     }
 }
