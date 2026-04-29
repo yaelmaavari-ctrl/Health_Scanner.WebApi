74cca50b0d1ada7e7dd6f3c0f2753b47bd8e219f
Yael <yaelmaavari@gmail.com>
2026-04-29 17:13:41 +0300

    feat(service): implement IScanHistoryService for managing user scans

---
+++ Service/Interfaces/IScanHistoryService.cs
@@ -0,0 +1,15 @@
+﻿using Service.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interfaces
+{
+    public interface IScanHistoryService
+    {
+        Task<IEnumerable<ScanHistoryDto>> GetUserHistory(int userId);
+        Task AddToHistory(int userId, ScanResultDto scanResult, string barcode);
+    }
+}
---
+++ Service/Services/ScanHistoryService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Repository.Entities;
+using Repository.Interfaces;
+using Service.Dto;
+using Service.Interfaces;
+
+public class ScanHistoryService(IScanHistoryRepository scanHistoryRepository, IMapper mapper) : IScanHistoryService
+{
+    private readonly IScanHistoryRepository _scanHistoryRepository = scanHistoryRepository;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<IEnumerable<ScanHistoryDto>> GetUserHistory(int userId)
+    {
+        var historyEntities = await _scanHistoryRepository.GetUserHistory(userId);
+
+        return _mapper.Map<IEnumerable<ScanHistoryDto>>(historyEntities);
+    }
+
+    public async Task AddToHistory(int userId, ScanResultDto scanResult, string barcode)
+    {
+        if (scanResult?.ScannedProduct != null)
+        {
+            var historyEntry = new ScanHistory
+            {
+                UserId = userId,
+                Barcode = barcode,
+                ProductName = scanResult.ScannedProduct.ProductName,
+                ImageUrl = scanResult.ScannedProduct.ImageUrl,
+                IsSafe = scanResult.ScannedProduct.IsSafeForUser,
+                ScannedAt = DateTime.Now
+            };
+            await _scanHistoryRepository.AddScan(historyEntry);
+        }  
+    }
+}
