433b235aeaf103bf0ddf532b7a4b40875401faea
Yael <yaelmaavari@gmail.com>
2026-04-29 17:14:07 +0300

    feat(service): integrate scan history recording into ProductService

---
+++ Service/Interfaces/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Service.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interfaces
+{
+    public interface IProductService
+    {
+        Task<ProductResponseDto> GetProductByBarcode(string barcode, List<string> forbiddenAllergens);
+        Task<List<ProductResponseDto>> GetAlternativeProducts(List<string> categories, List<string> forbiddenAllergens, string currentNutriscore);
+        Task<ScanResultDto> ScanAndProcessProduct(string barcode, List<string> forbiddenAllergens, int userId);
+    }
+
+}
