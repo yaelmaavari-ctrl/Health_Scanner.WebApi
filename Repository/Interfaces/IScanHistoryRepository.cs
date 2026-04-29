2b155e8b518791e670c5b17f73c37b26eaad2fd9
Yael <yaelmaavari@gmail.com>
2026-04-29 16:59:17 +0300

    feat(repository): implement core ScanHistory data access

---
+++ Repository/Interfaces/IScanHistoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IScanHistoryRepository
+    {
+        Task AddScan(ScanHistory scan);
+        Task<IEnumerable<ScanHistory>> GetUserHistory(int userId, int limit = 10);
+    }
+}
---
+++ Repository/Repositories/ScanHistoryRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Entities;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories
+{
+    public class ScanHistoryRepository(IContext context) : IScanHistoryRepository
+    {
+        private readonly IContext _context = context; 
+
+        public async Task AddScan(ScanHistory scan)
+        {
+            await _context.ScanHistories.AddAsync(scan);
+            await _context.Save();
+        }
+
+        public async Task<IEnumerable<ScanHistory>> GetUserHistory(int userId, int limit = 10)
+        {
+            return await _context.ScanHistories
+                .Where(s => s.UserId == userId)
+                .OrderByDescending(s => s.ScannedAt)
+                .Take(limit)
+                .ToListAsync();
+        }
+    }
+}
+
+
+
+
